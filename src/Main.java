34530c629f95cffc2c36ce72abb2b3f924f83ade
Mark <voronkov1mark@gmail.com>
2023-11-30 00:39:05 +0300

    создание условий вызывающих исключение и его обработка в блоке try-catch

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
-import java.io.Reader;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-
-
+        try {
+            divide(6, 0);
+        } catch (ArithmeticException e) {
+            System.out.println("на ноль делить нельзя!");
+        }
     }
+
+    static void divide(int a, int b) {
+        System.out.println(a/b);
+    }
+
 }
 
